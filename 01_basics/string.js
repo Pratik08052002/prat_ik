c4db780e76eb7e4c1cce32df6db60ac74d6f1a5d
Pratik08052002 <156571827+Pratik08052002@users.noreply.github.com>
2024-01-17 10:13:35 +0000

    strings in js

---
+++ 01_basics/string.js
@@ -0,0 +1,12 @@
+const gamename=new String('hitesh')
+// console.log(gamename[0])
+// console.log(gamename)
+// console.log(gamename.length);
+// console.log(gamename.charAt(4));
+console.log(gamename.indexOf('h'));
+console.log(gamename.substring(0,4));
+const newstring="    hitesh    ";
+console.log(gamename.trim());
+const anotherstring=gamename.slice(-8,4);
+console.log(anotherstring);
+
