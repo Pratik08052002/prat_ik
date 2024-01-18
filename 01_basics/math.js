6fc885847ddf2e2a8a706870ee3ac65ef8dfdad1
Pratik08052002 <156571827+Pratik08052002@users.noreply.github.com>
2024-01-18 08:33:48 +0000

    math and num

---
+++ 01_basics/math.js
@@ -0,0 +1,19 @@
+//numbers in javascript
+// const balance=new Number(100.567);
+// console.log(balance);
+// console.log(balance.toFixed(2));//. ke baad round off
+// const othernum=23.8966
+// console.log(othernum.toPrecision(3));//precision . se pehle round off but can do after point also based on number
+// const hundred=10000000;
+// console.log(hundred.toLocaleString('en-In'));//this function converts us standard to indian standard
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//maths in js
+// console.log (Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.random(12.0));//gives value 0 to 1
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
