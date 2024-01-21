6b01fb2cfaf1decbc172d6991516b91e3b41e483
Pratik08052002 <156571827+Pratik08052002@users.noreply.github.com>
2024-01-21 12:41:27 +0000

    object2,3

--- 02_b/object2.js
+++ 02_b/object2.js
@@ -1,11 +1,11 @@
 // const tinderuser =newobject()
 // console.log(tinderuser);
-const tinderuser={}
-tinderuser.name="ashima"
-tinderuser.id="123abc"
-tinderuser.age="34"
-tinderuser.isloggedIn=false
-console.log(tinderuser);
+// const tinderuser={}
+// tinderuser.name="ashima"
+// tinderuser.id="123abc"
+// tinderuser.age="34"
+// tinderuser.isloggedIn=false
+// console.log(tinderuser);
 //access of nested objects
 // const regularuser={
 //     email:"cheenu@gmail.com",
@@ -29,7 +29,18 @@ console.log(tinderuser);
 //onather famous method spreadmethod
 // const obj3={...obj1,...obj2,...obj4};
 // console.log(obj3);
-console.log(Object.keys(tinderuser));
-console.log(Object.values(tinderuser));//object ka data type aaray h issey ham mainupulate kr skte h loops m
-console.log(tinderuser.hasOwnProperty.apply('isloggedin'));
+// console.log(Object.keys(tinderuser));
+// console.log(Object.values(tinderuser));//object ka data type aaray h issey ham mainupulate kr skte h loops m
+// console.log(tinderuser.hasOwnProperty.apply('isloggedin'));
+//
 
+//object part 3 destructure
+const course={
+coursename:"js in hindi",
+priceprice:"999",
+courseinstructor:"hitesh"
+}
+const{courseinstructor:instructor}=course;
+console.log(instructor);
+const{priceprice:p}=course
+console.log(p);
